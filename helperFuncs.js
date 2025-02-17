34e284bdfa9241b3c54d2fc292d2ea347c269b62
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2025-02-16 20:37:49 -0800

    Update helperFuncs.js

--- helperFuncs.js
+++ helperFuncs.js
@@ -103,7 +103,7 @@ function UpdateRenderTasks() {
 }
 
 function updateBtnHandler() {
-    if (!currentGoalInput.value.trim() || !timeInput.value) {
+    if (!currentGoalInput.value || !timeInput.value) {
         alert('fill out form')
         return
     }
