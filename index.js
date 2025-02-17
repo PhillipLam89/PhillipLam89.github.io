173a2b8128121ac1e05d422fcaff70ce2e809d69
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2025-02-16 20:07:56 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 let secondsPassedInDay = 0
 let lastTaskClickedOn = null
 let allTasks = [
-    {startHour: '10', startMinutes: '36', Goal: 'ERROR, SAMPLE GOAL 1', Done:false, index:0,
+    {startHour: '10', startMinutes: '36', Goal: 'Default Goal', Done:false, index:0,
                    isPM: true, trueValue: 22*60 + 36
-    },
-    {startHour: '3',startMinutes:'40', Goal: 'ERROR, SAMPLE GOAL 2', Done:false, index:1, isPM: true,
-                    trueValue: 15*60 + 40
-    },
-    {startHour: '1',startMinutes:'08', Goal:  'ERROR, SAMPLE GOAL 3', Done:false, index:2, isPM: false,
-                    trueValue: 1 + 8
-    
-    }    
+    }
 ]
 
 allTasks = JSON.parse(localStorage.getItem('data'))
