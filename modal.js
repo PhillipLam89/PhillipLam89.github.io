a25cde9792d777a65183e364b6083b645d62df25
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2025-02-16 20:55:12 -0800

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,24 +1,24 @@
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
+const modal = document.querySelector(".modal")
+const overlay = document.querySelector(".overlay")
+const openModalBtn = document.querySelector(".btn-open")
+const closeModalBtn = document.querySelector(".btn-close")
 
 // close modal function
 const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  modal.classList.add("hidden")
+  overlay.classList.add("hidden")
 };
 
 // close the modal when the close button and overlay is clicked
-closeModalBtn.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+closeModalBtn.addEventListener("click", closeModal)
+overlay.addEventListener("click", closeModal)
 
 // close modal when the Esc key is pressed
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
-});
+
+document.onkeydown = ({key}) => 
+  key == "Escape" && 
+  !modal.classList.contains("hidden") && 
+  closeModal()
 
 // open modal function
 const defaultModalHTML =  
