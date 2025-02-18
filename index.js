9485363b257832c96462cfd4899bb14876551e36
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2025-02-17 20:49:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ let secondsPassedInDay = 0
 let lastTaskClickedOn = null
 let allTasks = [
     {startHour: '10', startMinutes: '36', Goal: 'Default Goal', Done:false, index:0,
-                   isPM: true, trueValue: 22*60 + 36
+                   isPM: true, trueValue: 22*60 + 36,
+                   startTimeAMPM: '14:30', endTimeAMPM: '17:31',
+                   startTimeSecs:0, endTimeSecs:0
     }
 ]
 
@@ -10,12 +12,10 @@ allTasks = JSON.parse(localStorage.getItem('data'))
          || allTasks
 window.onload = function runOnBoot() { //loads current date
             updateToNewDay()
-            UpdateRenderTasks()
+            renderTasksHTML()
             updateBtn.onclick  = updateBtnHandler
             this.setInterval(updateTime, 999)
 
             //see helperFuncs.js for other functions
 }
-
-
 
