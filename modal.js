0db963c22f98c6f46dc9f8fec324307524132269
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2025-02-21 20:21:08 -0800

    Update modal.js

--- modal.js
+++ modal.js
@@ -56,9 +56,91 @@ const infoModalHTML =
        <li>Task less than 1 hr away will have  <span style="color: red;">red countdown</span></li>
    </div>`
 
+
+const setAlarmModal = 
+ ` <div class="flex"> 
+        <button class="btn-close">⨉</button>
+   </div>
+   <br>
+   <div id="taskParentWrapper">
+      <label for="newAlarmTimer">New Alarm</label>
+      <input id="newAlarmTimer" type="time">
+      <br>
+      <br>
+      <button id="updateBtnAlarms" class="btn">Update!</button>
+   </div>`
+
+
+let allAlarms = [];
+
+console.log(localStorage.getItem('alarms'))
+
+allAlarms = JSON.parse(localStorage.getItem('alarms')) || allAlarms
+function handleAlarmUpdateBtn(e) {
+  let id = e.target.id
+  if (!newAlarmTimer.value) {
+    alert('invalid time')
+    return
+  }
+  let alertTimeForTmr = false
+  let timeInSeconds = inputValueToSeconds(newAlarmTimer.value)
+  let isPM = newAlarmTimer.value.slice(0,2) > 11 ? 'PM' : 'AM'
+
+  if (secondsPassedInDay > timeInSeconds) {
+    alertTimeForTmr = true
+    timeInSeconds = (86400 - (secondsPassedInDay - timeInSeconds))
+  } else {timeInSeconds = (timeInSeconds - secondsPassedInDay )}
+  
+  
+  let startTimeAPM = newAlarmTimer.value
+  let startingHour = startTimeAPM.slice(0,2)
+  let startingMins = startTimeAPM.slice(3)
+      startingHour = startingHour == 0 ? '12' : startingHour
+      startingHour = startingHour > 12 ? String(startingHour - 12) : startingHour
+      startingHour+=':'
+  const timeObj = {countdownTimeSecs: timeInSeconds,
+                   countdownTimeHrs: timeInSeconds / 3600,
+                  startTimeAMPM: startingHour + startingMins, 
+                  isForTmr: alertTimeForTmr, isPM:isPM , isActive: true,
+                  index: allAlarms.length
+                  };
+        
+  allAlarms.push(timeObj) 
+  bigDaddyWrapper.insertAdjacentHTML('afterbegin', renderAlarmsHTML())
+ 
+
+
+  closeModal()
+  localStorage.setItem('alarms', JSON.stringify(allAlarms)) 
+
+}
+function renderAlarmsHTML() {
+  let format = ''
+  bigDaddyWrapper.innerHTML = ''
+  allAlarms.forEach((alarm,i) => {
+   format+= `
+     <section id=alarm-${i}-wrapper>
+      <h1>alarm: ${alarm.startTimeAMPM} ${alarm.isPM}</h1>
+      <input type="checkbox" id="alarm-${i}-toggle"></h3>
+     </section>
+   
+   `
+  })
+
+  return format
+} 
 const openModal = function (EventPassedIn = false) {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
+  
+  if (EventPassedIn == 'setAlarmBtn')  {
+    modal.innerHTML = setAlarmModal
+    const closeModalBtn = document.querySelector(".btn-close");
+    closeModalBtn.addEventListener("click", closeModal);
+    updateBtnAlarms.onclick = handleAlarmUpdateBtn
+    return
+  }
+
   modal.innerHTML = EventPassedIn ? infoModalHTML : defaultModalHTML
   if(window.updateBtn) updateBtn.onclick  = updateBtnHandler;
 
